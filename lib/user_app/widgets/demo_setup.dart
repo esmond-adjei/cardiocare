cfa316f7b1064929b968533dc4d45a15f828a93c
esmond-adjei <xmongram@gmail.com>
2024-09-02 13:41:02 +0000

    git commit =m 'idk'

---
+++ lib/user_app/widgets/demo_setup.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class DemoSettingsScreen extends StatelessWidget {
+  const DemoSettingsScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
